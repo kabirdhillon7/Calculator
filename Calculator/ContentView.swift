20dbdb54dd7e3e3368866da2c9cce333c7bc8801
Kabir Dhillon <kabirdhillon99@gmail.com>
2023-01-18 00:15:16 -0800

    Update ContentView.swift
    Completed operator functionality

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -40,9 +40,15 @@ enum CalcButton: String {
     }
 }
 
+enum Operation {
+    case add, subtrack, multiply, divide, none
+}
+
 struct ContentView: View {
     
     @State var value = "0"
+    @State var runningNumber = 0
+    @State var currentOperator: Operation? = .none
     
     var buttons: [[CalcButton]] = [
         [.clear, .negative, .percent, .divide],
@@ -65,7 +71,7 @@ struct ContentView: View {
                     
                     Text(value)
                         .bold()
-                        .font(.system(size: 72))
+                        .font(.system(size: 100))
                         .foregroundColor(.white)
                 }
                 .padding()
@@ -75,7 +81,7 @@ struct ContentView: View {
                     HStack(spacing: 12) {
                         ForEach(row, id: \.self) { item in
                             Button(action: {
-                                
+                                self.didTap(button: item)
                             }, label: {
                                 Text(item.rawValue)
                                     .font(.system(size: 32))
@@ -96,7 +102,51 @@ struct ContentView: View {
     }
     
     func didTap(button: CalcButton) {
-        
+        switch button {
+        case .add, .subtract, .divide, .multiply, .equal:
+            if button == .add {
+                self.currentOperator = .add
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .subtract {
+                self.currentOperator = .subtrack
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .multiply {
+                self.currentOperator = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .divide {
+                self.currentOperator = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .equal {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                
+                switch self.currentOperator {
+                case .add: self.value = "\(runningValue + currentValue)"
+                case .subtrack: self.value = "\(runningValue - currentValue)"
+                case .multiply: self.value = "\(runningValue * currentValue)"
+                case .divide: self.value = "\(runningValue / currentValue)"
+                case .none:
+                    break
+                case .some(.none):
+                    break
+                }
+            }
+            if button != .equal {
+                self.value = "0"
+            }
+        case .clear:
+            self.value = "0"
+            break
+        case .decimal, .negative, .percent:
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0"{
+                value = number
+            } else {
+                self.value = "\(self.value)\(number)"
+            }
+        }
     }
     
     func buttonWidth(item: CalcButton) -> CGFloat {
@@ -110,8 +160,6 @@ struct ContentView: View {
         return (UIScreen.main.bounds.width - (5*12)) / 4
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
